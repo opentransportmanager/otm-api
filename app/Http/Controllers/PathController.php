fca41b9eb0951c4dbbe2b4b041d492e4adb52b07
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-15 18:11:53 +0200

    Created Path domain and db migration

---
+++ app/Http/Controllers/PathController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Path;
+use Illuminate\Http\Request;
+
+class PathController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Path  $path
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Path $path)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Path  $path
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Path $path)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Path  $path
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Path $path)
+    {
+        //
+    }
+}
