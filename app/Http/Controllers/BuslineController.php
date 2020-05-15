99987ace1130c8d40bf5bb9cd1db494170bb4a36
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-15 17:05:10 +0200

    Created Busline domain and db migration

---
+++ app/Http/Controllers/BuslineController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Busline;
+use Illuminate\Http\Request;
+
+class BuslineController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Busline  $busline
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Busline $busline)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Busline  $busline
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Busline $busline)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Busline  $busline
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Busline $busline)
+    {
+        //
+    }
+}
