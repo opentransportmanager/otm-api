8466e17e16a19a4f209900d7c51f41dbc7a8755c
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-15 18:54:35 +0200

    Fix for station position datatype with workaround for POINT type

--- database/migrations/2020_05_15_152722_create_stations_table.php
+++ database/migrations/2020_05_15_152722_create_stations_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateStationsTable extends Migration
 {
@@ -13,12 +14,15 @@ class CreateStationsTable extends Migration
      */
     public function up()
     {
+
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('address');
             $table->timestamps();
         });
+        /*Fix for inconsistent schema blueprint for $table->point() using PostGIS geography(point,) type instead
+         Detailed explanation: https://github.com/laravel/framework/issues/32183 */
+        DB::statement('ALTER TABLE stations ADD COLUMN position POINT');
     }
 
     /**
