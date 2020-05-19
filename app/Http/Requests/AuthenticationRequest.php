2db8b177a4b1f367b4d6785e72f00e6aca178a80
PR <patrykreszczynski@gmail.com>
2020-05-19 18:56:13 +0200

    AuthenticationRequest

---
+++ app/Http/Requests/AuthenticationRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthenticationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|string',
+            'password' => 'required|string'
+        ];
+    }
+}
