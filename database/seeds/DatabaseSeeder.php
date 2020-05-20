b9681334a3bd02fdfe01b9de97e325dd64255932
PR <patrykreszczynski@gmail.com>
2020-05-20 11:20:23 +0200

    Fixed Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,8 +11,8 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,16 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
 
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin')
-        ]);
+        $items = [
+            ['name' => 'admin', 'email' => 'admin@example.com', 'password' => Hash::make('admin')]
+        ];
+        foreach ($items as $item){
+            User::updateOrCreate(['email' => $item['email']], $item);
+        }
     }
 }
