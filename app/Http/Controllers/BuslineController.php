8b17ab75180bbb89f19e16e95da04dfc84552b85
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-22 14:15:51 +0200

    Added BuslineController functioning methods and FormRequests

--- app/Http/Controllers/BuslineController.php
+++ app/Http/Controllers/BuslineController.php
@@ -5,26 +5,31 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Busline;
+use App\Http\Requests\StoreBusline;
+use App\Http\Requests\UpdateBusline;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class BuslineController extends Controller
 {
     /**
-     * Show the form for creating a new resource.
+     * Display all resources.
      */
-    public function create(): JsonResponse
+    public function index(): JsonResponse
     {
-        return response()->json([], Response::HTTP_NOT_IMPLEMENTED);
+        $buslines = Busline::all();
+
+        return response()->json([$buslines], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): JsonResponse
+    public function store(StoreBusline $request): JsonResponse
     {
-        return response()->json([], Response::HTTP_NOT_IMPLEMENTED);
+        Busline::create($request->validated());
+
+        return response()->json(['message' => 'Busline created succesfully'], Response::HTTP_CREATED);
     }
 
     /**
@@ -32,15 +37,17 @@ class BuslineController extends Controller
      */
     public function show(Busline $busline): JsonResponse
     {
-        return response()->json([], Response::HTTP_NOT_IMPLEMENTED);
+        return response()->json($busline, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Busline $busline): JsonResponse
+    public function update(UpdateBusline $request, Busline $busline): JsonResponse
     {
-        return response()->json([], Response::HTTP_NOT_IMPLEMENTED);
+        $busline->update($request->validated());
+
+        return response()->json(['message' => 'Busline updated succesfully'], Response::HTTP_OK);
     }
 
     /**
@@ -48,6 +55,8 @@ class BuslineController extends Controller
      */
     public function destroy(Busline $busline): JsonResponse
     {
-        return response()->json([], Response::HTTP_NOT_IMPLEMENTED);
+        $busline->delete();
+
+        return response()->json(['message' => 'Busline deleted successfully'], Response::HTTP_OK);
     }
 }
