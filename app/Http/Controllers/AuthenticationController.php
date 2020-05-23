948c8c30cf5993f024ddac4796094d3fe7a27dcc
PR <patrykreszczynski@gmail.com>
2020-05-23 18:21:24 +0200

    Response Fix in Authentication Controller

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AuthenticationRequest;
 use App\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
@@ -16,16 +17,16 @@ class AuthenticationController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json('These credentials do not match our records.', 404);
+            return response()->json('These credentials do not match our records.', Response::HTTP_NOT_FOUND);
         }
 
-        $token = $user->createToken('my-app-token')->plainTextToken;
+        $token = $user->createToken('otm-token')->plainTextToken;
 
         $response = [
             'user' => $user,
             'token' => $token
         ];
 
-        return response()->json($response, 201);
+        return response()->json($response, Response::HTTP_OK);
     }
 }
