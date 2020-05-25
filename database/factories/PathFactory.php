cf2132a0fa6b151cf587a03c02d9b1b4b7ab46ce
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-25 17:43:42 +0200

    Removed obsolete parameter in factory definition

--- database/factories/PathFactory.php
+++ database/factories/PathFactory.php
@@ -7,7 +7,7 @@ use App\Path;
 use Faker\Generator as Faker;
 
 /* @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Path::class, function (Faker $faker, $buslines): array {
+$factory->define(Path::class, function (Faker $faker): array {
     return [
         'busline_id' => Busline::all()->random()->id,
         'created_at' => $faker->dateTime,
