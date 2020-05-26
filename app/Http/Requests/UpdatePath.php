b2e48a644bdace7827ef26534b41ab20c5e23a11
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-05-26 16:34:51 +0200

    Adds CRUD functionality and validation for Path model

---
+++ app/Http/Requests/UpdatePath.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+class UpdatePath extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'busline_id' => 'required|exists:buslines,id',
+        ];
+    }
+}
