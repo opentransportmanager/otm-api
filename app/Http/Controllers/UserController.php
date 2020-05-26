03c0de336d284e1db70ccdb20b6566380659ac6d
PR <patrykreszczynski@gmail.com>
2020-05-26 19:23:03 +0200

    Created UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,61 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUser;
+use App\Http\Requests\UpdateUser;
+use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Lists all resources.
+     */
+    public function index(): JsonResponse
+    {
+        $users = User::all();
+
+        return response()->json($users, Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUser $request, RegisterService $service): JsonResponse
+    {
+        User::create($request->validated());
+
+        return response()->json(['message' => 'User created successfully'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): JsonResponse
+    {
+        return response()->json($user, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUser $request, User $user): JsonResponse
+    {
+        $user->update($request->validated());
+
+        return response()->json(['message' => 'User updated succesfully'], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        return response()->json([], Response::HTTP_OK);
+    }
 }
