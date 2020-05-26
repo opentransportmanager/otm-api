922b0cb06a902591d89ab1cb6c92067217b270e0
PR <patrykreszczynski@gmail.com>
2020-05-26 19:23:40 +0200

    Created RegistrationService

---
+++ app/Services/RegistrationService.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegistrationService
+{
+    public function registerUser(array $input): array
+    {
+
+    }
+}
