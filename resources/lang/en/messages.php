af3c482698ce91d816cd9fc54443f4eb6b351fc3
PR <patrykreszczynski@gmail.com>
2020-05-28 15:36:48 +0200

    messages fix

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,33 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-'group' => [
-    'created' => 'Group has been created successfully',
-    'updated' => 'Group has been updated successfully',
-    'deleted' => 'Group has been deleted succesfully',
-],
-'station' => [
-    'created' => 'Station has been created successfully',
-    'updated' => 'Station has been updated successfully',
-    'deleted' => 'Station has been deleted succesfully',
-],
-'busline' => [
-    'created' => 'Busline has been created successfully',
-    'updated' => 'Busline has been updated successfully',
-    'deleted' => 'Busline has been deleted succesfully',
-],
-'path' => [
-    'created' => 'Path has been created successfully',
-    'updated' => 'Path has been updated successfully',
-    'deleted' => 'Path has been deleted succesfully',
-],
-'path_station' => [
-    'attached' => 'Stations attached successfully',
-    'detached' => 'Stations detached successfully',
-],
-'user' => [
-    'created' => 'User has been created successfully',
-    'updated' => 'User has been updated successfully',
-    'deleted' => 'User has been deleted succesfully',
-],
+    'group' => [
+        'created' => 'Group has been created successfully',
+        'updated' => 'Group has been updated successfully',
+        'deleted' => 'Group has been deleted succesfully',
+    ],
+    'station' => [
+        'created' => 'Station has been created successfully',
+        'updated' => 'Station has been updated successfully',
+        'deleted' => 'Station has been deleted succesfully',
+    ],
+    'busline' => [
+        'created' => 'Busline has been created successfully',
+        'updated' => 'Busline has been updated successfully',
+        'deleted' => 'Busline has been deleted succesfully',
+    ],
+    'path' => [
+        'created' => 'Path has been created successfully',
+        'updated' => 'Path has been updated successfully',
+        'deleted' => 'Path has been deleted succesfully',
+    ],
+    'path_station' => [
+        'attached' => 'Stations attached successfully',
+        'detached' => 'Stations detached successfully',
+    ],
+    'user' => [
+        'created' => 'User has been created successfully',
+        'updated' => 'User has been updated successfully',
+        'deleted' => 'User has been deleted succesfully',
+    ],
 ];
