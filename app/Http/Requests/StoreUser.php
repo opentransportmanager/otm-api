ae4beea072592e37c9daeb65cf0d853a11794ab0
PR <patrykreszczynski@gmail.com>
2020-05-28 14:25:32 +0200

    Added BaseRequest

--- app/Http/Requests/StoreUser.php
+++ app/Http/Requests/StoreUser.php
@@ -4,20 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class StoreUser extends FormRequest
+class StoreUser extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
