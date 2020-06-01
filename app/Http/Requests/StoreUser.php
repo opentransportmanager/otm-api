328c25fb7bfe26729d639c08ddc9d35d537434ae
PR <patrykreszczynski@gmail.com>
2020-06-01 13:15:07 +0200

    StoreUser extends UpdateUser

--- app/Http/Requests/StoreUser.php
+++ app/Http/Requests/StoreUser.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-class StoreUser extends BaseRequest
+class StoreUser extends UpdateUser
 {
     /**
      * Get the validation rules that apply to the request.
      */
     public function rules(): array
     {
+        $rules = parent::rules();
+
         return [
-            'name' => 'required|string|min:3|max:20',
+            'name' => $rules['name'],
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ];
