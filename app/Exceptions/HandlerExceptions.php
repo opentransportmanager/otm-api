9def1761c91c06e0837f3da474a786da5cf59055
PR <patrykreszczynski@gmail.com>
2020-06-08 20:38:00 +0200

    Added MethodNotAllowed and TokenMismatch exceptions

--- app/Exceptions/HandlerExceptions.php
+++ app/Exceptions/HandlerExceptions.php
@@ -7,6 +7,8 @@ namespace App\Exceptions;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 trait HandlerExceptions
@@ -14,15 +16,19 @@ trait HandlerExceptions
     public function apiException($request, $exception): JsonResponse
     {
         if ($exception instanceof ModelNotFoundException) {
-            return response()->json([
-                    'error' => 'Model not found',
-                ], Response::HTTP_NOT_FOUND);
+            return response()->json(['error' => 'Model not found'], Response::HTTP_NOT_FOUND);
         }
 
         if ($exception instanceof NotFoundHttpException) {
-            return response()->json([
-                    'error' => 'Incorrect route',
-                ], Response::HTTP_NOT_FOUND);
+            return response()->json(['error' => 'Incorrect route'], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json(['error' => 'Incorrect method'], Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            return response()->json(['error' => 'Token mismatch'], Response::HTTP_FORBIDDEN);
         }
 
         return parent::render($request, $exception);
