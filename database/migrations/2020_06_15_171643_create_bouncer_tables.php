b55fb1af12f282b02187678f9c4269d318705026
PR <patrykreszczynski@gmail.com>
2020-06-16 12:29:55 +0200

    Strict types in Bouncer migration

--- database/migrations/2020_06_15_171643_create_bouncer_tables.php
+++ database/migrations/2020_06_15_171643_create_bouncer_tables.php
@@ -1,19 +1,18 @@
 <?php
 
-use Silber\Bouncer\Database\Models;
+declare(strict_types=1);
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Silber\Bouncer\Database\Models;
 
 class CreateBouncerTables extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create(Models::table('abilities'), function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -81,10 +80,8 @@ class CreateBouncerTables extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::drop(Models::table('permissions'));
         Schema::drop(Models::table('assigned_roles'));
