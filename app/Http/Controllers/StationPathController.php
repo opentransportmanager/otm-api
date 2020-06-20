affbc2c569f02b5a9356be5f3169e580912a1627
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-06-20 10:31:46 +0200

    Added features: showing station->paths and showing timetable

---
+++ app/Http/Controllers/StationPathController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Path;
+use App\Services\StationPathService;
+use App\Station;
+use Illuminate\Http\JsonResponse;
+
+class StationPathController extends Controller
+{
+    public function __construct(StationPathService $stationPathService)
+    {
+        $this->stationPathService = $stationPathService;
+    }
+
+    /**
+     * Show stations attached to a selected path.
+     */
+    public function showAttachedPaths(Station $station): JsonResponse
+    {
+        $paths = $this->stationPathService->showAttachedPaths($station);
+
+        return response()->json($paths);
+    }
+
+    /**
+     * Show timetable of selected Station on selected Path
+     */
+    public function showTimetable(Station $station, Path $path): JsonResponse
+    {
+        $timetable = $this->stationPathService->showTimetable($station, $path);
+
+        return response()->json($timetable);
+    }
+}
---
+++ app/Services/StationPathService.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Path;
+use App\PathStation;
+use App\Station;
+use Illuminate\Support\Carbon;
+
+class StationPathService
+{
+    /**
+     * Shows Paths attached to selected Station.
+     */
+    public function showAttachedPaths(Station $station): array
+    {
+        $paths = $station->paths;
+
+        foreach ($paths as $path) {
+            $path->setAttribute('busline_number', $path->busline->number);
+        }
+
+        return $paths->toArray();
+    }
+
+    /**
+     * Shows timetable based on selected Station and Path combination.
+     */
+    public function showTimetable(Station $station, Path $path): array
+    {
+        $travel_time = PathStation::findIntermediateModel($path->id, $station->id)->travel_time;
+        $timetable = $path->courses;
+        foreach ($timetable as $course) {
+            $departure_time = Carbon::parse($course->start_time);
+            $departure_time->addMinutes($travel_time);
+            $course->setAttribute('departure_time', $departure_time->format(config('formats.hours_minutes')));
+        }
+
+        return $timetable->toArray();
+    }
+}
