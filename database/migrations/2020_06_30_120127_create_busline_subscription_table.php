a51985b470034903ba70dfcebdc33004a64881f0
PR <patrykreszczynski@gmail.com>
2020-06-30 14:10:31 +0200

    Added busline_subscription migration

---
+++ database/migrations/2020_06_30_120127_create_busline_subscription_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuslineSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('busline_subscription', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('busline_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('busline_subscription');
+    }
+}
