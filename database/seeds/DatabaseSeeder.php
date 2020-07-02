6acf3b18c39367e9e772a24e339621c734784f8e
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-07-02 21:10:03 +0200

    Added safe production seeders and added seeding to docker setup

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,17 +11,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (App::environment('local', 'development', 'staging')) {
-            $this->call([
-                BuslineSeeder::class,
-                StationSeeder::class,
-                GroupSeeder::class,
-                PathSeeder::class,
-                CourseSeeder::class,
-                PathStationSeeder::class,
-                BouncerSeeder::class,
-                UsersTableSeeder::class,
-            ]);
+        if (App::environment('local', 'testing')) {
+            $this->call(
+                [
+                    BouncerSeeder::class,
+                    UsersTableSeeder::class,
+                ]
+            );
+        }
+
+        if (App::environment('testing')) {
+            $this->call(
+                [
+                    BuslineSeeder::class,
+                    StationSeeder::class,
+                    GroupSeeder::class,
+                    PathSeeder::class,
+                    CourseSeeder::class,
+                    PathStationSeeder::class,
+                ]
+            );
+        }
+
+        if (App::environment('production', 'staging')) {
+            $this->call(
+                [
+                    PolicyProductionSeeder::class,
+                    UserProductionSeeder::class,
+                ]
+            );
         }
     }
 }
--- phpdocker/php-fpm/setup.sh
+++ phpdocker/php-fpm/setup.sh
@@ -1,7 +1,7 @@
 composer install
 cp -n .env.example .env
 php artisan key:generate
-until php artisan migrate;
+until php artisan migrate:refresh --seed;
 do echo "Retrying to migrate database";
 sleep 2;
 done
