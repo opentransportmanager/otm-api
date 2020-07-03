e54c1b69287cbbd50d356689d10e83df5f4227b4
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-07-03 11:18:44 +0200

    Factories code cleanup

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 use App\Group;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-/* @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Group::class, function (Faker $faker): array {
-    return [
-        'name' => $faker->safeColorName.' '.$faker->dayOfWeek,
-        'created_at' => $faker->dateTime,
-        'updated_at' => $faker->dateTime,
-    ];
-});
+/* @var Factory $factory */
+$factory->define(
+    Group::class,
+    function (Faker $faker): array {
+        return [
+            'name' => $faker->safeColorName . ' ' . $faker->dayOfWeek,
+            'created_at' => $faker->dateTime,
+            'updated_at' => $faker->dateTime,
+        ];
+    }
+);
