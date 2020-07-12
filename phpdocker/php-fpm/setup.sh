1259d8a5cdc13e9a1d9fd10629cddf4cddb2f3a3
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-07-12 23:11:58 +0200

    Added cron job to setup service startup script. Setup service is persistent now.

--- phpdocker/php-fpm/setup.sh
+++ phpdocker/php-fpm/setup.sh
@@ -1,8 +1,12 @@
-composer install
+composer install &&
 cp -n .env.example .env
 php artisan key:generate
-until php artisan migrate:refresh --seed;
+until php artisan migrate:fresh --seed;
 do echo "Retrying to migrate database";
 sleep 2;
 done
-echo "Migration process done!"
+echo "Migration process done!" &&
+crontab -r &&
+(crontab -l 2>/dev/null; echo "* * * * * cd /application && php artisan schedule:run >> /dev/null 2>&1") | crontab - &&
+service cron restart &&
+tail -f /dev/null
